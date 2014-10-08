211529fc05564b0d49a7ed63edff5a785a4c28ad
Sifiso Bhembe <stillpledged@gmail.com>
2014-10-08 19:00:45 +0200

    after home customization

--- ffcc/Controllers/HomeController.vb
+++ ffcc/Controllers/HomeController.vb
@@ -5,15 +5,4 @@
         Return View()
     End Function
 
-    Function About() As ActionResult
-        ViewData("Message") = "Your application description page."
-
-        Return View()
-    End Function
-
-    Function Contact() As ActionResult
-        ViewData("Message") = "Your contact page."
-
-        Return View()
-    End Function
 End Class
